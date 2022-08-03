b0690b6dac6746b9c678e5593603433af1b5fda5
mm4te <mateopatatian@gmail.com>
2022-08-03 16:36:26 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,186 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#000000';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+
+window.addEventListener('scroll', function(){
+	let animacion = this.document.getElementById('animacion');
+	let posicionObj1 = animacion.getBoundingClientRect().top;
+	let tamañoDePantalla = window.innerHeight/5;
+	/* console.log(posicionObj1) */
+	if(posicionObj1 < tamañoDePantalla){
+		animacion.style.animation = 'desplazar2 1.5s '
+	}
+	
+})
+
+window.sr = ScrollReveal();
+  sr.reveal('.navbar', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-500px'
+});
+window.sr = ScrollReveal();
+  sr.reveal('.navbar2', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-500px'
+});
+
+window.sr = ScrollReveal();
+  sr.reveal('.navbar3', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-500px'
+});
+
+window.sr = ScrollReveal();
+  sr.reveal('.navbar4', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-500px'
+});
+
+
+
+
+
+window.addEventListener('load', () => {
+	const contenedor_loader = document.querySelector('.contenedor_loader')
+	contenedor_loader.style.opacity = 0
+	contenedor_loader.style.visibility = 'hidden'
+
+}) 
+
+document.addEventListener('DOMContentLoaded', function () {
+  let figure = document.getElementById('photo');
+  let directions = document.getElementById('directions');
+
+  directions.addEventListener('change', function () {
+    figure.setAttribute('tooltip-dir', this.value);
+  });
+});
+
+
+// set the starting position of the cursor outside of the screen
+let clientX = 100;
+let clientY = 100;
+const innerCursor = document.querySelector(".cursor--small");
+
+const initCursor = () => {
+  // add listener to track the current mouse position
+  document.addEventListener("mousemove", e => {
+    clientX = e.clientX;
+    clientY = e.clientY;
+  });
+  
+  // transform the innerCursor to the current mouse position
+  // use requestAnimationFrame() for smooth performance
+  const render = () => {
+    innerCursor.style.transform = `translate(${clientX}px, ${clientY}px)`;
+    // if you are already using TweenMax in your project, you might as well
+    // use TweenMax.set() instead
+    // TweenMax.set(innerCursor, {
+    //   x: clientX,
+    //   y: clientY
+    // });
+    
+    requestAnimationFrame(render);
+  };
+  requestAnimationFrame(render);
+};
+
+initCursor();
+
+let lastX = 0;
+let lastY = 0;
+let isStuck = false;
+let showCursor = false;
+let group, stuckX, stuckY, fillOuterCursor;
+
+const initCanvas = () => {
+  const canvas = document.querySelector(".cursor--canvas");
+  const shapeBounds = {
+    width: 75,
+    height: 75
+  };
+  paper.setup(canvas);
+  const strokeColor = "rgb(10, 145, 255)";
+  const strokeWidth = 1;
+  const segments = 8;
+  const radius = 15;
+  
+  // we'll need these later for the noisy circle
+  const noiseScale = 150; // speed
+  const noiseRange = 4; // range of distortion
+  let isNoisy = false; // state
+  
+  // the base shape for the noisy circle
+  const polygon = new paper.Path.RegularPolygon(
+    new paper.Point(0, 0),
+    segments,
+    radius
+  );
+  polygon.strokeColor = strokeColor;
+  polygon.strokeWidth = strokeWidth;
+  polygon.smooth();
+  group = new paper.Group([polygon]);
+  group.applyMatrix = false;
+  
+  const noiseObjects = polygon.segments.map(() => new SimplexNoise());
+  let bigCoordinates = [];
+  
+  // function for linear interpolation of values
+  const lerp = (a, b, n) => {
+    return (1 - n) * a + n * b;
+  };
+  
+  // function to map a value from one range to another range
+  const map = (value, in_min, in_max, out_min, out_max) => {
+    return (
+      ((value - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+    );
+  };
+  
+  // the draw loop of Paper.js 
+  // (60fps with requestAnimationFrame under the hood)
+  paper.view.onFrame = event => {
+    // using linear interpolation, the circle will move 0.2 (20%)
+    // of the distance between its current position and the mouse
+    // coordinates per Frame
+    lastX = lerp(lastX, clientX, 0.2);
+    lastY = lerp(lastY, clientY, 0.2);
+    group.position = new paper.Point(lastX, lastY);
+  }
+}
+
+initCanvas();
+
+
+
+
